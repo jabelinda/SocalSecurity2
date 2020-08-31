0723fd381a2acb3f9da1619c26101cf453e3b3ba
JabeLinda <jabelinda@outlook.com>
2020-08-31 15:39:03 +0200

    tillåter inparametrar

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Globalization;
+using System.Net.NetworkInformation;
 
 namespace SocalSecurity2
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Please enter Social Security Number (YYMMDD--XXXX)");
 
-            string socialSecurityNumber = Console.ReadLine();
+        {
+            
+            string socialSecurityNumber=SocialSecurityNumber();
 
             string genderNumberString = socialSecurityNumber.Substring(socialSecurityNumber.Length - 2, 1);
 
@@ -29,15 +30,88 @@ namespace SocalSecurity2
 
             string birthDateString = socialSecurityNumber.Substring(0, 6);
 
-            DateTime birthDate = DateTime.ParseExact(birthDateString, "YYMMDD", CultureInfo.InvariantCulture);
+            DateTime birthDate = DateTime.ParseExact(birthDateString, "yyMMdd", CultureInfo.InvariantCulture);
 
             int age = DateTime.Now.Year - birthDate.Year;// DateTime.Today.Year
 
-            if (birthDate.Month > DateTime.Today.Month || birthDate.Month==DateTime.Today.Month&& birthDate.Day>DateTime.Now.Day) 
+            if (birthDate.Month > DateTime.Today.Month || birthDate.Month == DateTime.Today.Month && birthDate.Day > DateTime.Now.Day)
             {
                 age--;//age = age - 1;
             }
             Console.WriteLine($"{gender},{age}");
+        }
+
+        public static string SocialSecurityNumber()
+        {
+            Console.WriteLine("Please enter Social Security Number (YYMMDD--XXXX)");
+            string socialSecurityNumber = Console.ReadLine();
+            return socialSecurityNumber;
+        }
+        public static string SocialSecurityNumber (string socialSecurityNumber)
+        {
+            return socialSecurityNumber;  
+
+           
+        }
+
+    }
+}
+
+
+namespace SocialSecurityNumber_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string socialSecurityNumber;
+
+            // Input
+            if (args.Length > 0)
+            {
+                // if input from terminal is already done
+                Console.WriteLine($"You provided: {args[0]}");
+                socialSecurityNumber = args[0];
+            }
+            else
+
+            {   // Ask for input
+                Console.WriteLine("Please input your Social security number YYMMDD-XXXX");
+                socialSecurityNumber = Console.ReadLine();
+            }
+
+
+            // Gender
+            string genderNumberString = socialSecurityNumber.Substring(socialSecurityNumber.Length - 2, 1);
+
+            int genderNumber = int.Parse(genderNumberString);
+
+            string gender;
+
+            if (genderNumber % 2 == 0) // True/false  (Boolean)
+            {
+                gender = "Female";
+            }
+            else
+            {
+                gender = "Male";
+            }
+
+            // Age
+            string birthDateString = socialSecurityNumber.Substring(0, 6);
+
+            DateTime birthDate = DateTime.ParseExact(birthDateString, "yyMMdd", CultureInfo.InvariantCulture);
+
+            int age = DateTime.Now.Year - birthDate.Year;
+
+            if (birthDate.Month > DateTime.Today.Month || birthDate.Month == DateTime.Now.Month && birthDate.Day > DateTime.Now.Day)
+            {
+                age = age - 1;
+            }
+
+
+            // Result presentation
+            Console.WriteLine($"This is a: {gender}, with age: {age}");
         }
     }
 }
